81380ec72f04d66c6440951017323d3d1e00b0af
MateusBraz <silvabraz96@gmail.com>
2022-05-21 15:46:29 -0300

    cross cutting service registry discovery

--- course/src/main/java/com/ead/course/clients/AuthUserClient.java
+++ course/src/main/java/com/ead/course/clients/AuthUserClient.java
@@ -35,20 +35,19 @@ public class AuthUserClient {
 
     public Page<UserDto> getAllUsersByCourse(UUID courseId, Pageable pageable){
         List<UserDto> searchResult = null;
-        ResponseEntity<ResponsePageDto<UserDto>> result = null;
         String url = REQUEST_URL_AUTHUSER + utilsService.createUrlGetAllUsersByCourse(courseId, pageable);
         log.debug("Request URL: {} ", url);
         log.info("Request URL: {} ", url);
         try{
             ParameterizedTypeReference<ResponsePageDto<UserDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<UserDto>>() {};
-            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            ResponseEntity<ResponsePageDto<UserDto>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
             searchResult = result.getBody().getContent();
             log.debug("Response Number of Elements: {} ", searchResult.size());
         } catch (HttpStatusCodeException e){
             log.error("Error request /courses {} ", e);
         }
         log.info("Ending request /users courseId {} ", courseId);
-        return result.getBody();
+        return new PageImpl<>(searchResult);
     }
 
     public ResponseEntity<UserDto> getOneUserById(UUID userId){
